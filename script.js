a8dfdcf285afde9911660d9d16ee5a78b124e362
Svalyan <you@example.com>
2022-04-26 23:53:10 +0600

    slider not  work

--- script.js
+++ script.js
@@ -7,6 +7,38 @@ let inputEmail = document.querySelector('.form__input-email');
 let inputSubject = document.querySelector('.form__input-subject');
 let inputMessage = document.querySelector('.form__input-massage');
 
+
+let offset = 0 // смещение от левого края
+const sliderLine = document.querySelector('.slider__wrapper-line');
+
+console.log(offset);
+
+function left() {
+    console.log(offset + 'left()');
+    offset++;
+    sliderLine.style.left = -offset + 'px';
+}
+
+
+document.querySelector(`.slider__button-right`).addEventListener('click', function (){
+    if (offset % 256 == 0) {
+        stop();
+    }else {
+        console.log(offset + 'else');
+        setInterval(left, 10);
+    }
+});
+
+
+document.querySelector('.slider__button-left').addEventListener('click', function (){
+    offset = offset + 256;
+    if (offset > 768) {
+        offset = 0;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
+
+
 /*помещаем все импуты в массив*/
 let inputArr = [inputName, inputVenueName, inputVenueCity, inputStateName, inputEmail, inputSubject, inputMessage];
 
