db035b596201e91f8eedb05732e7e9ccaa1cb2da
Svalyan <you@example.com>
2022-05-14 22:17:52 +0600

    enough for today

--- script.js
+++ script.js
@@ -1,30 +1,19 @@
 /*получили инпуты*/
-let inputName = document.querySelector('.form__input-name');
-let inputVenueName = document.querySelector('.form__input-venueName');
-let inputVenueCity = document.querySelector('.form__input-venueCity');
-let inputStateName = document.querySelector('.form__input-stateName');
-let inputEmail = document.querySelector('.form__input-email');
-let inputSubject = document.querySelector('.form__input-subject');
-let inputMessage = document.querySelector('.form__input-massage');
+const inputs = document.querySelectorAll('.selectInput');
 
-/*Красим границы инпутов в красный*/
-/*помещаем все импуты в массив*/
-let inputArr = [inputName, inputVenueName, inputVenueCity, inputStateName, inputEmail, inputSubject, inputMessage];
-
+/*Красим границы инпутов в красный путем удаления одного стиля и добавления нового*/
+function getRed() {
+    inputs.forEach((itemInput, indexInput) => {
+        itemInput.classList.remove('inputBorderW');
+        itemInput.classList.add('inputBorderRed');
+    })
+}
 /*получили кнопку*/
 let btnSend = document.querySelector('.form__button');
-/*повесили на нее слушатель события click, и выполнением функции changeStyle*/
-btnSend.addEventListener( 'click', changeStyle, false)
+/*повесили на нее слушатель события click, и выполнением функции getRed*/
+btnSend.addEventListener( 'click', getRed, false)
 
-/*описали собственно функцию*/
-function changeStyle() {
-    for (let i = 0; i < inputArr.length; i++) {
-        /*удаляем у всех элементов стиль отвечающий за цвет границы инпута*/
-        inputArr[i].classList.remove('inputBorderW');
-        /*добавляем всем новый стиль отвечающий за красный цвет границы инпута*/
-        inputArr[i].classList.add('inputBorderRed');
-    }
-}
+
 
 /**************************СЛАЙДЕР**********************************/
 const prev = document.querySelector(`.slider__button-right`);
